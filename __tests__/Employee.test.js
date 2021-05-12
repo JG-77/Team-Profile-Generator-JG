42a7be53e1f6e88dbfd1d76c35fb06234fd6ba71
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-05-11 20:06:51 -0700

    Got tests to pass for employee class.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,19 +1,34 @@
-// const { it } = require('@jest/globals');
-// const { describe } = require('yargs');
-// const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 
 describe('Employee class', () => {
-    //describe('getName', () => {
-        it("should diplay the employee's name", () => {
+    //Test for getName()
+    it("should diplay the employee's name", () => {
 
-            expect(new Employee('Sample Name').getName()).toBe('Sample Name');
-        });
-        it("should return error if no input is given", () => {
+        expect(new Employee('Sample Name').getName()).toBe('Sample Name');
+    });
+    it("should return error if no input is given", () => {
 
-            expect(new Employee().getName()).toBe(undefined);
-        });
+        expect(new Employee().getName()).toBe(undefined);
+    });
 
+    //Test for getId()
+    it("should display employee's ID", () => {
+        // const employee = new Employee('Jon',10);
+        // expect(employee.getId()).toBe('number');
+        expect(new Employee('Jon','17').getId()).toBe('17');
+    });
+     it("should return error if no input is given", () => {
 
-    //})
+        expect(new Employee().getId()).toBe(undefined);
+    });
+
+    //Test for getEmail()
+    it("should display employee's email", () => {
+
+        expect(new Employee('Jon','17','Sample@email.com').getEmail()).toBe('Sample@email.com');
+    });
+     it("should return error if no input is given", () => {
+
+        expect(new Employee().getEmail()).toBe(undefined);
+    });
 })
