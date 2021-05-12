2fc37e136fa520f849b85c8b1553a8732c9625ce
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-05-11 21:42:30 -0700

    Engineer.js and test file set and tests passing.' git push

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -5,10 +5,15 @@ describe('Engineer class getgitHub()', () => {
      //Test for getGitHub()
     it("should diplay the engineer's github URL", () => {
 
-        expect(new Engineer('Sample-user').getGitHub()).toBe('https://github.com/Sample-user');
+        expect(new Engineer('Jon','17','Sample@email.com','Sample-user').getGitHub()).toBe('Sample-user');
     });
     it("should return error if no input is given", () => {
 
         expect(new Engineer().getGitHub()).toBe(undefined);
+    });
+    //Test getRole()
+    it("should diplay the employee's role", () => {
+
+        expect(new Engineer().getRole()).toBe('Engineer');
     });
 });
