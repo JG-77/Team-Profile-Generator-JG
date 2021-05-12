6371a20ad5274afa43d9a480a6bbdcf7c662e0d0
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-05-11 18:46:26 -0700

    set up employee class and set up emplyee.test.js

---
+++ Tests/Employee.test.js
@@ -0,0 +1,19 @@
+const { it } = require('@jest/globals');
+const { describe } = require('yargs');
+const { expect } = require('@jest/globals');
+const Employee = require('../lib/Employee');
+
+describe('Employee class', () => {
+    describe('getName', () => {
+        it("should diplay the employee's name", () => {
+
+            expect(new Employee('Sample Name').getName('Sample Name')).toBe(true);
+        });
+        it("should return error if no input is given", () => {
+
+            expect(new Employee('Sample Name').getName('')).toBe(false);
+        });
+
+        
+    })
+})
