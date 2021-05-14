9eb9ad1ca5566afc283f81dce78ea1e67b30a873
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-05-13 19:57:08 -0700

    Created all prompt functions for user data input

--- src/prompt.js
+++ src/prompt.js
@@ -4,11 +4,8 @@ const Engineer = require('../lib/Engineer');
 const Intern = require('../lib/Intern');
 const Manager = require('../lib/Manager');
 
-// class AskPrompt {
+// class AskPrompt {} will I need a class?
 
-//     constructor () {
-
-//     }
     // Prompt asking employee name
     function askEmployeeName() {
         inquirer.prompt([
@@ -28,52 +25,99 @@ const Manager = require('../lib/Manager');
             })
     }
     // Prompt asking employee ID
-    // askEmployeeId() {
-    //     inquirer
-    //         .prompt([
-    //             {
-    //             type: 'input', 
-    //             name: 'id',
-    //             message: "What is this employee's ID?",
-    //             },
-    //         ])
-    //         .then(data =>{
-                
-    //         })
-    // }
-    // // Prompt asking employee email    
-    // askEmployeeEmail() {
-    //     inquirer
-    //         .prompt([
-    //             {
-    //             type: 'input', 
-    //             name: 'email',
-    //             message: "What is this employee's email?",
-    //             },
-    //         ])
-    //          .then(data =>{
-                
-    //         })
-    // }
-    // // Prompt asking employee role
-    // askRole() {
-    //     inquirer
-    //         .prompt([
-    //             {
-    //             type: 'list', 
-    //             name: 'role',
-    //             message: "What is this employee's role?",
-    //             choices: ['Manager', 'Engineer', 'Intern']
-    //             },
-    //         ])
-    //         .then(data =>{
-                
-    //         })
-    // }
+    function askEmployeeId() {
+        inquirer
+            .prompt([
+                {
+                type: 'input', 
+                name: 'id',
+                message: "What is this employee's ID?",
+                validate: data => /[a-z1-9]/gi.test(data), 
+                },
+            ])
+            .then(data =>{
+                console.log(data.id)
+            })
+    }
+    // Prompt asking employee email    
+    function askEmployeeEmail() {
+        inquirer
+            .prompt([
+                {
+                type: 'input', 
+                name: 'email',
+                message: "What is this employee's email?",
+                },
+            ])
+             .then(data =>{
+                console.log(data.email)
+            })
+    }
+    // Prompt asking employee role
+    function askRole() {
+        inquirer
+            .prompt([
+                {
+                type: 'list', 
+                name: 'role',
+                message: "What is this employee's role?",
+                choices: ['Manager', 'Engineer', 'Intern'] //choice will then lead to one of the following functions
+                },
+            ])
+            .then(data =>{
+                console.log(data.role)
+            })
+    }
 
-        
-        
-        
-    //}
-askEmployeeName();
-    
+     // Prompt asking Manager office number (if user selected manager)
+    function askManagerOffice() {
+        inquirer
+            .prompt([
+                {
+                type: 'input', 
+                name: 'office',
+                message: "What is this manager's office number?",
+                },
+            ])
+            .then(data =>{
+                console.log(data.office)
+            })
+    }   
+      
+    // Prompt asking Engineer GitHub Username (if user selected engineer)
+    function askEngineerGitHub() {
+        inquirer
+            .prompt([
+                {
+                type: 'input', 
+                name: 'gitHub',
+                message: "What is this engineer's GitHub username?",
+                },
+            ])
+            .then(data =>{
+                console.log(data.gitHub)
+            })
+    }   
+
+    // Prompt asking Intern's school (if user selected intern)
+    function askInternSchool() {
+        inquirer
+            .prompt([
+                {
+                type: 'input', 
+                name: 'school',
+                message: "At what school is this intern studying?",
+                },
+            ])
+            .then(data =>{
+                console.log(data.school)
+            })
+    } 
+
+// askEmployeeName();
+// askEmployeeId();
+// askEmployeeEmail();
+// askRole();
+// askManagerOffice();
+// askEngineerGitHub();
+// askInternSchool();
